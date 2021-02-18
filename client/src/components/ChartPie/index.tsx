b2bb504d3a96fd2abc0c13ab38a1df390e960e00
lcsoares10 <lucassoares81@hotmail.com>
2021-02-17 21:13:46 -0300

    perf: Modified component to use lib victory to generate graphics

--- client/src/components/ChartPie/index.tsx
+++ client/src/components/ChartPie/index.tsx
@@ -1,40 +1,28 @@
 import React from 'react'
-import { Pie } from 'react-chartjs-2'
+import { VictoryPie } from 'victory'
+// x e y = quantidadee
+
+type dataPie = {
+  x: number | string
+  y: number
+}
 
 type PropsChartPie = {
-  data: Array<number>
+  data: Array<dataPie>
   labels: Array<string>
   backgroundColor: Array<string>
   title?: string
 }
 
 const ChartPie: React.FC<PropsChartPie> = props => {
-  const data = {
-    labels: props.labels,
-    datasets: [
-      {
-        backgroundColor: props.backgroundColor,
-        data: props.data,
-        borderColor: 'rgba(255, 255, 255, 0.574)',
-        borderWidth: '1'
-      }
-    ]
-  }
   return (
-    <Pie
-      data={data}
-      options={{
-        title: {
-          display: true,
-          text: props.title ? props.title : '',
-          fontSize: 20
-        },
-        borderColor: 'rgba(0, 0, 0, 0.1)',
-        legend: {
-          display: false,
-          position: 'right'
-        }
-      }}
+    <VictoryPie
+      colorScale={props.backgroundColor}
+      data={props.data}
+      radius={50}
+      padAngle={5}
+      innerRadius={120}
+      style={{ labels: { fill: 'white', fontSize: 20, fontWeight: 'bold' } }}
     />
   )
 }
