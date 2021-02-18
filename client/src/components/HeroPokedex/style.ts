8860ff31b700907557c0ea2c1b3acf19ae0a37e7
lcsoares10 <lucassoares81@hotmail.com>
2021-02-17 21:18:09 -0300

    style: Clean component style

--- client/src/components/HeroPokedex/style.ts
+++ client/src/components/HeroPokedex/style.ts
@@ -5,40 +5,14 @@ export const Hero = styled.div`
   align-items: center;
   flex-direction: column;
   margin-top: 50px;
+  width: 100%;
 `
 
 export const ArticleHero = styled.article`
   margin-top: 50px;
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  width: 100%;
+  flex-wrap: wrap;
 `
 
-export const Chart = styled.div`
-  width: 100%;
-`
-export const Subtitle = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  width: 100%;
-`
-export const SubtitleLine = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  justify-content: space-between;
-  width: 100%;
-`
-export const SubtitleItem = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  p {
-    font-size: 1rem;
-  }
-  img {
-    width: 30px;
-  }
-`
+export const Chart = styled.div``
