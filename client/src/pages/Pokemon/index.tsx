7a9a9b6404bb5c587defd48ded0143edaa7f9327
lcsoares10 <lucassoares81@hotmail.com>
2021-02-19 19:59:41 -0300

    feat: Created page Pokemon

---
+++ client/src/pages/Pokemon/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Header from '../../components/Header'
+import HeroPokedex from '../../components/HeroPokedex'
+import Main from '../../components/Main'
+
+const Pokedex: React.FC = () => {
+  return <></>
+}
+
+export default Pokedex
