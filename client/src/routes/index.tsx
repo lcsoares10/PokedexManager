fdfe33bda83349f16c66323735abf3869b1fe527
lcsoares10 <lucassoares81@hotmail.com>
2021-02-19 20:01:27 -0300

    feat: Add route Pokemon

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -4,10 +4,13 @@ import Error404 from '../pages/Error404'
 import Login from '../pages/Login'
 import Home from '../pages/Home'
 import Pokedex from '../pages/Pokedex'
+import Pokemon from '../pages/Pokemon'
+
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/pokemon" component={Pokemon} />
         <Route path="/myPokedex" component={Pokedex} />
         <Route path="/home" component={Home} />
         <Route exact path="/" component={Login} />
