1d3ea498a05da5e3d880fcd97a41bd83681a4324
lcsoares10 <lucassoares81@hotmail.com>
2021-02-19 12:48:13 -0300

    refactor: Adapted component to use class MyTypesPokemons

--- client/src/components/HeroPokedex/index.tsx
+++ client/src/components/HeroPokedex/index.tsx
@@ -1,53 +1,27 @@
 import React, { useMemo } from 'react'
 import Title from '../Title'
 import ChartPie from '../ChartPie'
-import getTypeColors from './helper/getTypeColorsPokemon'
+import MyTypesPokemon from '../../helper/MyTypesPokemon'
 import LegendTypesPokemon from '../LegendTypesPokemon'
 
 import * as S from './style'
 
 const HeroPokedex: React.FC = () => {
-  const names = [
-    'Normal',
-    'Poison',
-    'Steel',
-    'Ice',
-    'Ghost',
-    'Electric',
-    'Ground',
-    'Psychic',
-    'Gass',
-    'Dragon',
-    'Water',
-    'Flying',
-    'Bug',
-    'Ice',
-    'Dark',
-    'Fire',
-    'Rock',
-    'Fighting'
-  ]
   const myTypes = [
     { name: 'Dragon', x: 1, y: 1 },
     { name: 'Gass', x: 4, y: 4 },
     { name: 'Fire', x: 3, y: 3 },
     { name: 'Fighting', x: 2, y: 2 }
   ]
-  const backgroundColor = useMemo(() => getTypeColors({ myTypes: myTypes }), [
-    names
-  ])
+  const myTypesPokemon = new MyTypesPokemon({ myTypes })
+  const backgroundColor = useMemo(() => myTypesPokemon.getColors(), [myTypes])
 
   return (
     <S.Hero>
       <Title>POKÉDEX</Title>
       <S.ArticleHero>
-        {' '}
         <S.Chart>
-          <ChartPie
-            data={myTypes}
-            labels={names}
-            backgroundColor={backgroundColor}
-          />
+          <ChartPie data={myTypes} backgroundColor={backgroundColor} />
         </S.Chart>
         <LegendTypesPokemon />
       </S.ArticleHero>
