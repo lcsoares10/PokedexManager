0b1ecd78b02aaf56c5bdcdf63b7488df6c22a2b0
lcsoares10 <lucassoares81@hotmail.com>
2021-02-19 23:41:01 -0300

    feat:  Making the API call on the component

--- client/src/components/Main/index.tsx
+++ client/src/components/Main/index.tsx
@@ -3,8 +3,17 @@ import Container from '../Container'
 import Title from '../Title'
 import * as S from './style'
 import ListPokemons from '../ListPokemons'
+import client from '../../services/graphql/client'
+import useSWR from 'swr'
+import { RequestDocument } from 'graphql-request/dist/types'
+import GET_ALL_POKEMONS from '../../services/graphql/queries/getAllpokemons'
 
 const Main: React.FC = () => {
+  const fetcher = (query: RequestDocument) =>
+    client.request(query, { limit: 10 })
+
+  const { data, error } = useSWR(GET_ALL_POKEMONS, fetcher)
+
   return (
     <Container>
       <S.Main>
