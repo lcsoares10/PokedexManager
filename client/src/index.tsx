95e363ef7420620af43750c1a5ed3b36ce66e472
lcsoares10 <lucassoares81@hotmail.com>
2021-02-19 21:16:35 -0300

    feat: Providing redux store on application

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,10 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { Provider } from 'react-redux'
+
+import { themeReducer } from './redux/themeReducer'
+import { createStore } from 'redux'
+const store = createStore(themeReducer)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
