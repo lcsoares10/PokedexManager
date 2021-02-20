f93dc531267718b84e56e327bf63558186be37df
lcsoares10 <lucassoares81@hotmail.com>
2021-02-20 19:19:13 -0300

    refector : Using helper to search for Pokémon

--- client/src/components/ListPokemons/index.tsx
+++ client/src/components/ListPokemons/index.tsx
@@ -2,40 +2,29 @@ import React from 'react'
 import styled from 'styled-components'
 
 import Card from '../CardPokemon'
-import client from '../../services/graphql/client'
-import useSWR from 'swr'
-import { RequestDocument } from 'graphql-request/dist/types'
-import GET_ALL_POKEMONS from '../../services/graphql/queries/getAllpokemons'
-import searchPokemon from '../../utils/searchPokemon'
+
+import searchPokemon from '../../helper/searchPokemon'
 import { useSearch } from '../../context/Search'
+import { pokemonType } from '../../services/graphql/api/types/pokemon'
 
 export const List = styled.div`
   margin-top: 80px;
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: space-between;
-  width: 100%;
+  justify-content: flex-start;
   flex-wrap: wrap;
 `
 const ListPokemons: React.FC = () => {
   const { search } = useSearch()
-  const fetcher = async (query: RequestDocument) =>
-    await client.request(query, { limit: 1500 })
-
-  const { data, error } = useSWR(GET_ALL_POKEMONS, fetcher)
-  if (!data) {
-    return <p>Aguarde</p>
-  }
-  const pokemons = { ...data.pokemons }
-  const pokemonsFound = searchPokemon(pokemons.results, search)
+  const pokemonsFound = searchPokemon(search)
 
   return (
     <React.Fragment>
       {pokemonsFound && (
         <List>
-          {pokemonsFound.map((pokemon: any, key: any) => (
-            <Card key={key} />
+          {pokemonsFound.map((pokemon: pokemonType, key: any) => (
+            <Card key={key} pokemon={pokemon} />
           ))}
         </List>
       )}
