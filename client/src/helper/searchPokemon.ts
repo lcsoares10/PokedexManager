a441edfc5ccd4c6dab88292cae52af28d31a01fc
lcsoares10 <lucassoares81@hotmail.com>
2021-02-20 19:22:57 -0300

    feat: Helper created to filter request

---
+++ client/src/helper/searchPokemon.ts
@@ -0,0 +1,29 @@
+import { useFetch } from '../hook/useFetch'
+import GET_ALL_POKEMONS from '../services/graphql/queries/getAllpokemons'
+
+import {
+  pokemonType,
+  pokemonListType
+} from '../services/graphql/api/types/pokemon'
+
+function searchPokemon(search: string): Array<pokemonType> {
+  const { data, error } = useFetch(GET_ALL_POKEMONS)
+  const searchclean = search.trim().toLowerCase()
+
+  if (!data) {
+    return []
+  }
+
+  const pokemons: pokemonListType = { ...data.pokemons }
+
+  if (searchclean.length === 0) {
+    return pokemons.results.filter((pokemon: pokemonType) => {
+      return pokemon.id < 12 ? pokemon : ''
+    })
+  }
+
+  return pokemons.results.filter((pokemon: pokemonType) => {
+    return pokemon.name.indexOf(search) !== -1
+  })
+}
+export default searchPokemon
--- client/src/utils/searchPokemon.ts
+++
@@ -1,6 +0,0 @@
-function searchPokemon(pokemons: any, search: any) {
-  return pokemons.filter((pokemon: any) => {
-    return pokemon.name.indexOf(search) !== -1
-  })
-}
-export default searchPokemon
