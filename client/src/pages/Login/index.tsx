9fb7d5e394a787f37726973fdfb6ada14a28e81d
lcsoares10 <lucassoares81@hotmail.com>
2021-02-20 19:43:58 -0300

    feat: Add  username in localstorage

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from '../../components/Container'
 import { Link, useHistory } from 'react-router-dom'
 import * as S from './components/styles'
 
 const Login: React.FC = () => {
   const history = useHistory()
+  const [inputName, setInputName] = useState<string>('')
 
   const handleInput = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    localStorage.setItem('userName', inputName)
     history.push('/home')
   }
 
@@ -15,7 +17,11 @@ const Login: React.FC = () => {
     <Container style={{ height: '100vh' }}>
       <S.H1>Pokédex Manager</S.H1>
       <S.Form onSubmit={event => handleInput(event)}>
-        <S.Input type="text" placeholder="Digite seu nome" />
+        <S.Input
+          type="text"
+          placeholder="Digite seu nome"
+          onChange={e => setInputName(e.currentTarget.value)}
+        />
         <S.Button>
           <Link to="/home">Entrar</Link>
         </S.Button>
