27d40fe719ebc162917d3cf2ac8903eb14d68a21
lcsoares10 <lucassoares81@hotmail.com>
2021-02-20 15:04:24 -0300

    feat: Using search context hook to change status

--- client/src/components/Search/index.tsx
+++ client/src/components/Search/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import * as S from './style'
 import SendBlack from '../../assets/images/send_black.svg'
-// import { Container } from './styles';
+import { useSearch } from '../../context/Search'
 
 const Search: React.FC = () => {
+  const { search, setSearch } = useSearch()
   return (
     <S.BoxSearch>
-      <S.Input type="text" placeholder="Procurar o pókemons" />
+      <S.Input
+        type="text"
+        placeholder="Procurar o pókemons"
+        value={search}
+        onChange={e => setSearch(e.currentTarget.value)}
+      />
       <S.ButtonSend>
         <img src={SendBlack} alt="" />
       </S.ButtonSend>
