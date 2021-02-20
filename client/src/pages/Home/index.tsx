ccd4a164005be89f5cee649d564db62a86d38790
lcsoares10 <lucassoares81@hotmail.com>
2021-02-20 15:03:01 -0300

    feat:Providing context Search

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Header from '../../components/Header'
 import Main from '../../components/Main'
+import { SearchContext } from '../../context/Search'
 const Home: React.FC = () => {
+  const [search, setSearch] = useState('')
   return (
-    <>
+    <SearchContext.Provider value={{ search, setSearch }}>
       <Header />
       <Main />
-    </>
+    </SearchContext.Provider>
   )
 }
 
