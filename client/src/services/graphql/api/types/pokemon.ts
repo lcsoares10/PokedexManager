eb64a9fab98a3935999828142d4628946ece8a99
lcsoares10 <lucassoares81@hotmail.com>
2021-02-20 18:59:10 -0300

    refactor: Creating api type Pokémon

---
+++ client/src/services/graphql/api/types/pokemon.ts
@@ -0,0 +1,18 @@
+export type pokemonType = {
+  name: string
+  url?: string
+  image: string
+  id: number
+}
+
+export type pokemonListType = {
+  count?: number
+  next?: string
+  previous?: string
+  nextOffset?: number
+  prevOffset?: number
+  params?: string
+  results: Array<pokemonType>
+  status?: boolean
+  message?: string
+}
