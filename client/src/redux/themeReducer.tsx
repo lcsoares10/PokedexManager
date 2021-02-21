4f4192634c3d1c5495db263dc900eceed3c8b9da
lcsoares10 <lucassoares81@hotmail.com>
2021-02-20 23:04:32 -0300

    feat: Checking storage for theme definition

--- client/src/redux/themeReducer.tsx
+++ client/src/redux/themeReducer.tsx
@@ -1,16 +1,23 @@
 import * as Actions from './themeActions'
 import { darkTheme } from '../themes'
+
 const initialState = {
   theme: darkTheme
 }
+
 export const themeReducer = (
   state = initialState,
   action: Actions.themeAction
 ): typeof initialState => {
+  const themeLocalStorage = JSON.parse(localStorage.getItem('theme') as string)
+  const theme = !themeLocalStorage ? initialState : { theme: themeLocalStorage }
+
   switch (action.type) {
     case Actions.APPLY_THEME:
+      localStorage.setItem('theme', JSON.stringify(action.payload))
       return { theme: action.payload }
+
     default:
-      return state
+      return theme
   }
 }
