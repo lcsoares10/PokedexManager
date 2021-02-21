a40d50672808489aac96ad869bc8a7c6c32cbb94
lcsoares10 <lucassoares81@hotmail.com>
2021-02-21 13:02:10 -0300

    style: Adding capitalize

--- client/src/components/Title/index.tsx
+++ client/src/components/Title/index.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 const Title = styled.main`
   font-size: 3rem;
   color: ${props => props.theme.title};
+  text-transform: capitalize;
 `
 
 export default Title
