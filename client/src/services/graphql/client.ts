56b568777440e8cd7792f9547b3423e14fac2b3e
lcsoares10 <lucassoares81@hotmail.com>
2021-02-22 09:44:53 -0300

    refector: Add node env

--- client/src/services/graphql/client.ts
+++ client/src/services/graphql/client.ts
@@ -1,4 +1,5 @@
 import { GraphQLClient } from 'graphql-request'
-
-const client = new GraphQLClient(`${process.env.ENDPOINT}`, { headers: {} })
+const client = new GraphQLClient(process.env.REACT_APP_ENDPOINT as string, {
+  headers: {}
+})
 export default client
